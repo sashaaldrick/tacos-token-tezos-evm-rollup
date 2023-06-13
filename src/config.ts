b30c398327084732e4b99f2938a9d61cf339cf48
claudebarde <lotolosenc.oc@gmail.com>
2023-06-13 13:41:19 +0100

    Connected the dapp to the EVM rollup

--- src/config.ts
+++ src/config.ts
@@ -1,32 +1,13 @@
 import abi from "./assets/abi.json";
 
-// export default {
-//   chainId: "0x539",
-//   chainName: "Tezos EVM Rollup Dailynet",
-//   evmRollupUrl: () => {
-//     const date = new Date().toJSON().slice(0, 10);
-//     return `https://evm.dailynet-${date}.teztnets.xyz/`;
-//   },
-//   contractAddress: "0x9dD7efBfB908c3f8A96776390D186195bc5b9bD3",
-//   contractAbi: abi,
-//   decimals: 18,
-//   formatBalance: (balance: string): string => {
-//     if (isNaN(+balance)) throw "NaN";
-
-//     const balanceParts = balance.split(".");
-//     if (balanceParts.length === 1) {
-//       return balance;
-//     } else {
-//       return balanceParts[0] + "." + balanceParts[1].slice(0, 5);
-//     }
-//   }
-// };
-
 export default {
-  chainId: "0x13881",
-  chainName: "Polygon Mumbai",
-  evmRollupUrl: () => "https://polygon-testnet.public.blastapi.io",
-  contractAddress: "0x3b6bD43C8181c222B75DD728D21a17A74E9c605C",
+  chainId: "0x539",
+  chainName: "Tezos EVM Rollup Dailynet",
+  evmRollupUrl: () => {
+    const date = new Date().toJSON().slice(0, 10);
+    return `https://evm.dailynet-${date}.teztnets.xyz/`;
+  },
+  contractAddress: "0x9dD7efBfB908c3f8A96776390D186195bc5b9bD3",
   contractAbi: abi,
   decimals: 18,
   formatBalance: (balance: string): string => {
@@ -40,3 +21,22 @@ export default {
     }
   }
 };
+
+// export default {
+//   chainId: "0x13881",
+//   chainName: "Polygon Mumbai",
+//   evmRollupUrl: () => "https://polygon-testnet.public.blastapi.io",
+//   contractAddress: "0x3b6bD43C8181c222B75DD728D21a17A74E9c605C",
+//   contractAbi: abi,
+//   decimals: 18,
+//   formatBalance: (balance: string): string => {
+//     if (isNaN(+balance)) throw "NaN";
+
+//     const balanceParts = balance.split(".");
+//     if (balanceParts.length === 1) {
+//       return balance;
+//     } else {
+//       return balanceParts[0] + "." + balanceParts[1].slice(0, 5);
+//     }
+//   }
+// };
